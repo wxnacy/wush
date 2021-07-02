e6b0e9622e7ad9f015f6f445c2f1001abed7f39b
wxnacy <371032668@qq.com>
2021-07-02 12:30:58 +0800

    U: 完善地址补全模块

--- wapi/completion/filesystem.py
+++ wapi/completion/filesystem.py
@@ -17,7 +17,6 @@ class ExecutableCompleter(BaseCompleter):
 
     def get_completions(self, document, complete_event):
         super().get_completions(document, complete_event)
-        text = document.text
         last_word = document.text.split(' ')[-1]
         self.logger.info('last_word %s', last_word)
 
@@ -25,28 +24,42 @@ class ExecutableCompleter(BaseCompleter):
         dirname = os.path.dirname(path)
         self.logger.info('dirname %s', dirname)
 
-        word = self.word_before_cursor
         for name in os.listdir(dirname):
             self.logger.info('filename %s', name)
             if not self.filter_filename(name):
                 continue
             start_position = self.get_start_position()
             yield Completion(name, start_position=start_position)
-                         #  style='class:special-completion')
 
     def get_start_position(self):
         """获取文字补全的位置"""
-        if self.word_before_cursor in ('/', '~/'):
+        word = self.get_last_full_word(self.document.current_line_before_cursor)
+        if word in ('/', '~/'):
             return 0
-        return -len(self.word_before_cursor)
+        return -len(word)
 
     def filter_filename(self, name):
         """过滤文件名"""
         name = name.lower()
-        word = self.word_before_cursor.lower()
+        word = self.get_last_full_word(self.document.current_line_before_cursor)
         if word in ('/', '~/'):
             return True
         if name.startswith(word):
             return True
         return False
+
+    @classmethod
+    def get_last_full_word(cls, text):
+        """获取最后一个完整的单词"""
+        if text is None:
+            return None
+        text = text.strip()
+        if not text:
+            print('not', type(text), '-{}-'.format(text))
+            return text
+        text = text.split(' ')[-1]
+        if text in ('/', '~/'):
+            return text
+        last_text = text.split('/')[-1]
+        return last_text
 
