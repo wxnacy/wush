3dc1e1b6606f0d2e27ea27c255eee978d73473e2
wxnacy <wxnacy@gmail.com>
2021-12-14 00:00:07 +0800

    U: 补充 ready 脚本

--- bin/ready.sh
+++ bin/ready.sh
@@ -5,4 +5,9 @@
 pyenv virtualenv 3.6.10 wush
 pyenv local wush
 cp data/pip.conf $VIRTUAL_ENV/pip.conf
+pip install pyflakes
+pip install pytest
+pip uninstall pycrypto
+pip uninstall pycryptodome
+pip install pycryptodome
 pip install -r requirements.txt
