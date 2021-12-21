c47e399d7b1fed6bdb17729c65feaccf0521c079
wxnacy <xiaoning.wen@yiducloud.cn>
2021-12-21 12:50:42 +0800

    U: 去掉多余配置

--- wush/common/constants.py
+++ wush/common/constants.py
@@ -2,40 +2,11 @@
 # -*- coding:utf-8 -*-
 # Author: wxnacy@gmail.com
 """
-
+常量模块
 """
 
 import os
 import random
-
-# 默认配置目录
-CONFIG_USER_ROOT = 'tests/data/config'
-CONFIG_USER_ROOT = os.path.join(os.getenv("HOME"), '.wush')
-# 默认配置文件
-CONFIG_USER_PATH = os.path.join(CONFIG_USER_ROOT, 'config.yml')
-
-CONFIG_ROOT = CONFIG_USER_ROOT
-CONFIG_PATH = os.path.join(CONFIG_ROOT, 'config.yml')
-ENV_PATH = os.path.join(CONFIG_ROOT, 'env.yml')
-REQUEST_PATH = os.path.join(CONFIG_ROOT, 'request.yml')
-
-DEFAULT_SPACE_NAME = 'default'
-DEFAULT_MODULE_NAME = 'default'
-
-#  FUNC_NAME_GET_CURRENT_SPACE_NAME = 'get_current_space_name'
-
-#  DEFAULT_NAME_MODULE_ROOT = 'module_root'
-
-# 默认配置
-DEFAULT_CONFIG = {
-    'module_root': 'module',
-    'env_root': 'env',
-    'body_root': 'body',
-    'function_moduls': []
-}
-
-# request 请求方式
-METHODS = ('GET', 'OPTIONS', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE')
 
 class Constants(object):
     TMPDIR = '/tmp/wush'
